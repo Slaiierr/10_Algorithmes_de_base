1bedcef772776b2ade59f23b4efdf6e789fcbaea
Slaizer <12811281d@gmail.com>
2023-09-06 23:13:34 +0200

    ex 10 terminé

--- 10 Algorithmes de base/Ex 10.py
+++ 10 Algorithmes de base/Ex 10.py
@@ -1,10 +1,11 @@
 def occurence_lettres(phrase: str)-> dict:
     dico = {}
     for lettre in phrase:        
-        if dico[lettre] == False:
-            dico[lettre] = 1
-        if dico[lettre] == True:
+        if lettre in dico:
             dico[lettre] += 1
+        else:
+            dico[lettre] = 1
+           
     return dico
 
 assert occurence_lettres('Hello world !') == {'H': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 2, 'w': 1, 'r': 1, 'd': 1, '!': 1}, "La réponse est fausse !"
