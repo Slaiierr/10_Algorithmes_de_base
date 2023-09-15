44831f6dc6821fa301a0211e1db87f1947817d79
unknown <12811281d@gmail.com>
2023-09-15 09:00:35 +0200

    Modification des exos

--- 10 Algorithmes de base/Ex 3.py
+++ 10 Algorithmes de base/Ex 3.py
@@ -7,7 +7,6 @@ def delta(tab: list)->list:
     return liste
     
 
-
 assert delta([1000, 800, 802, 1000, 1003]) == [1000, -200, 2, 198, 3], "Le premier test n'est pas validé"
 assert delta([42]) == [42], "Le deuxième test n'est pas validé"
 assert delta([]) == None, "Le troisième test n'est pas validé"
--- 10 Algorithmes de base/Ex 8.py
+++ 10 Algorithmes de base/Ex 8.py
@@ -1,11 +1,30 @@
 def recherche(elt: int, tab: list)-> int:
-    for indice in range(len(tab)):
+    indice_found = -1
+    taille = len(tab) - 1
+    for indice in range(taille, -1, -1):
         if tab[indice] == elt:
-            return indice
-    return -1
+            indice_found = indice
+    return indice_found
         
 
 assert recherche(1, [2, 3, 4]) == -1, "Le premier test n'est pas validé"
 assert recherche(1, [10, 12, 1, 56]) == 2, "Le deuxième test n'est pas validé"
 assert recherche(50, [1, 50, 1]) == 1, "Le troisième test n'est pas validé"
 assert recherche(15, [8, 9, 10, 15]) == 3, "Le quatrième test n'est pas validé"
+
+
+def recherche_new(elt: int, tab: list) -> int:
+    occ = -1
+    trouve = False
+    taille = len(tab)
+    indice = 0
+    while (not trouve) and (indice < taille):
+        if elt == tab[indice]:
+            occ = indice
+        indice += 1
+    return occ
+
+assert recherche_new(1, [2, 3, 4]) == -1, "Le premier test n'est pas validé"
+assert recherche_new(1, [10, 12, 1, 56]) == 2, "Le deuxième test n'est pas validé"
+assert recherche_new(50, [1, 50, 1]) == 1, "Le troisième test n'est pas validé"
+assert recherche_new(15, [8, 9, 10, 15]) == 3, "Le quatrième test n'est pas validé"
