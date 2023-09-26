f87c3fd88f258702b2a55abdcc293ff13641798d
unknown <12811281d@gmail.com>
2023-09-26 08:23:17 +0200

    Modification exo 9

--- 10 Algorithmes de base/Ex 9.py
+++ 10 Algorithmes de base/Ex 9.py
@@ -3,10 +3,9 @@ def calcul(n: int)-> list:
     while n != 1:
         if n % 2 == 0:
             n = n / 2
-            liste.append(n)
         else:
             n = n * 3 + 1
-            liste.append(n)
+        liste.append(n)
     return liste
 
 assert calcul(7) == [7, 22, 11 , 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1], "Le résultat n'est pas correct"
